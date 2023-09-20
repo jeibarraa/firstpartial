5d708655e1fd9677efe091963b0d49fd8fbce810
jeibarraa <145053358+jeibarraa@users.noreply.github.com>
2023-09-19 21:36:46 -0600

    exam code.py

---
+++ exam code.py
@@ -0,0 +1,14 @@
+
+Exam
+#Function 1
+def name():
+  print("What is the price of the robot")
+  robot=input()
+  print("what is the discount")
+  discount=input()
+  price=100-int(discount)
+  Total=(int(robot)/100)*price
+  print("the final price would be:",Total)
+
+#Function 2
+name() 
