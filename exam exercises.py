d28d89bbb3f66eee8797d66c82b43ab61d22bfa2
jeibarraa <145053358+jeibarraa@users.noreply.github.com>
2023-09-19 21:37:52 -0600

    exam exercises.py

---
+++ exam exercises.py
@@ -0,0 +1,28 @@
+#Function 1
+def name():
+  print("¿Can you tell me the height of your rectangle?")
+  height=input()
+  print("tell me the wide of your rectangle?")
+  wide=input()
+  area=int(height)*int(wide)
+  print("the rectangle has an area of:",area)
+  #Function 2
+name()
+
+
+
+
+
+#Function 1
+def name():
+  print("¿Can you tell me the price of the hamburger?")
+  hamburger=input()
+  print("what is the discount?")
+  discount=input()
+  price=100-int(discount)
+  total=(int(hamburger)/100)*price
+  print("the total price would be of:", total)
+
+#Function 2
+name()
+
