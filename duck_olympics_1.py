7a3dc1b883d4bd442995a910359d38916f11a86b
jeibarraa <145053358+jeibarraa@users.noreply.github.com>
2023-10-06 13:51:43 -0600

    duck_olympics_1.py

---
+++ duck_olympics_1.py
@@ -0,0 +1,26 @@
+# cod_olimpiadas1.pyef def add(a,b): x = a+b return x
+def substraction(a,b): x = a-b return x
+print("Give me the first number")
+a = int(input())
+print("Give me the second number:") b = int(input())
+print("The addition gives",add(a,b),"and the substraction gives",susbtraction(a,b))
+
+def multiplication(a,b): x = a*b
+return x
+def division(a,b) x =a/b
+return x
+print("Give me the first number:")
+a = int(input())
+print("Give me the second number:") b = int(input())
+print("The rsult is", multiplication(a,b),"and the division", division (a,b))
+
+def conversion(kilometers) meters = kilometers * 1000 return meters
+kilometers = 10
+meters = conversion(kilometers)
+print("The conversion of meters to kilometers is", multiplication (kilometers*1000))
+
+def triangle_area (base,height): area = (base * height) / 2 return area
+base = 5
+height = 10
+area = triangle_area(base,area)
+ptint("The triangle's area is", multiplication(height ,base))
